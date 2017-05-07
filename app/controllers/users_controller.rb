2ac18def7a79dde4df029d38020c7e6a919a1431
kosukenaufan <k_yama1212@yahoo.co.jp>
2017-05-07 16:35:35 +0900

    titan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, only:[:destroy]
+  #before_action :require_login, only:[:destroy]
   #skip_before_filter :require_login, only: [:index, :new, :create, :activate]
   # GET /users
   # GET /users.json
